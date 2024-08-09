1496c415aaf4fc1d27b6c61ac32e021afa5d0c16
Oleksii Morozov <91106292+Capta1nAlex@users.noreply.github.com>
2024-08-09 16:01:52 +0200

    Update Farm & Heavy Construction Machinery.py
    Fixed a few typos, changed some variable names for better readability, fixed a bug with fedfund rows indexes, added wheat volume sold to the correlation, fixed bug regarding the output of the date of company's first financial report.

--- Farming Machinery prices correlation/Farm & Heavy Construction Machinery.py
+++ Farming Machinery prices correlation/Farm & Heavy Construction Machinery.py
@@ -5,11 +5,12 @@ import numpy as np
 import seaborn as sns
 
 # Read a CSV file into a DataFrame
-farmingCompanies = pd.read_csv('Farm.csv') #Date can be found in public 
+farmingCompanies = pd.read_csv('Farm.csv') #Data can be found in public sources
 fedfundsDF = pd.read_csv('FEDFUNDS.csv')
+wheatDF = pd.read_csv('Wheat.csv')
 farmingCompanies=farmingCompanies.dropna()
 fedfundsDF=fedfundsDF.dropna()
-
+wheatDF=wheatDF.dropna()
 def filter_and_drop(df, column_to_filter):
     # Convert the date column to datetime
     df[column_to_filter] = pd.to_datetime(df[column_to_filter])
@@ -25,47 +26,48 @@ def filter_and_drop(df, column_to_filter):
 # Apply the function to both DataFrames
 farmingCompanies = filter_and_drop(farmingCompanies, 'interval_date')
 fedfundsDF = filter_and_drop(fedfundsDF, 'DATE')
+wheatDF = filter_and_drop(wheatDF, 'Date')
 valid_months = [1, 4, 7, 10]
 
 # Extracting names of companies from file
 unique_names = set()
 farmingCompanies['interval_date'] = pd.to_datetime(farmingCompanies['interval_date'], infer_datetime_format=True)
-filtered_df = farmingCompanies[farmingCompanies['interval_date'].dt.year <= 2005]
-for name in filtered_df['ticker']:
+for name in farmingCompanies['ticker']:
     unique_names.add(name)
 
 for name in unique_names:
     fedfundsDF['DATE'] = pd.to_datetime(fedfundsDF['DATE'])
     fedfundsDF = fedfundsDF[fedfundsDF['DATE'].dt.month.isin(valid_months)]
-    tempdf=farmingCompanies[farmingCompanies['ticker'] == name]
-    print(farmingCompanies)
-    print(fedfundsDF)
+    wheatDF = wheatDF[wheatDF['Date'].dt.month.isin(valid_months)]
+    currentCompany=farmingCompanies[farmingCompanies['ticker'] == name]
     plt.figure(figsize=(8, 6))  # Adjust the figure size for better readability
     
-    corDF=tempdf[tempdf['item_name'] == "freeCashFlow"]
+    corDF=currentCompany[currentCompany['item_name'] == "freeCashFlow"]
     corDF = corDF.reset_index(drop=True)
     # Getting needed elements
-    dfTemp=tempdf[tempdf['item_name'] == "freeCashFlow"]
-    dfTemp = dfTemp.reset_index(drop=True)
-    corDF['freeCashFlow']=dfTemp['value']
+    tempColumn=currentCompany[currentCompany['item_name'] == "freeCashFlow"]
+    tempColumn = tempColumn.reset_index(drop=True)
+    corDF['freeCashFlow']=tempColumn['value']
 
-    dfTemp=tempdf[tempdf['item_name']== 'netIncome']
-    dfTemp = dfTemp.reset_index(drop=True)
-    corDF['netIncome']=dfTemp['value']
+    tempColumn=currentCompany[currentCompany['item_name']== 'netIncome']
+    tempColumn = tempColumn.reset_index(drop=True)
+    corDF['netIncome']=tempColumn['value']
 
-    dfTemp=tempdf[tempdf['item_name']== 'totalRevenue']
-    dfTemp = dfTemp.reset_index(drop=True)
-    corDF['totalRevenue']=dfTemp['value']
-    ratio = corDF[['freeCashFlow', 'netIncome', 'totalRevenue']]
-    print(dfTemp)
-    print(corDF)
-    ratio['FEDFUNDS'] = fedfundsDF['FEDFUNDS']
+    tempColumn=currentCompany[currentCompany['item_name']== 'totalRevenue']
+    tempColumn = tempColumn.reset_index(drop=True)
+    corDF['totalRevenue']=tempColumn['value']
     
+    tempColumn=fedfundsDF['FEDFUNDS']
+    tempColumn = tempColumn.reset_index(drop=True)
+    corDF['FEDFUNDS']=tempColumn
+    tempColumn=wheatDF['Volume']
+    tempColumn = tempColumn.reset_index(drop=True)
+    corDF['Volume']=tempColumn
     #creating correlation matrix + displaying/saving it
+    ratio = corDF[['freeCashFlow', 'netIncome', 'totalRevenue', 'FEDFUNDS', 'Volume']]
     corr_matrix = ratio.corr()
-    ratio
     sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', vmin=-1, vmax=1, linewidths=0.5, linecolor='black')
-    plt.legend(title='Fed funds start: ' + str(fedfundsDF.iloc[0,0].date()) + ' First financial report: ' + str(farmingCompanies.iloc[0,0].date()) + ' Grain prices:' + ' TODO', bbox_to_anchor=(-0.04, 1.15), loc='upper left')
+    plt.legend(title='Fed funds start: ' + str(currentCompany.iloc[0,0].date()) + ' First financial report: ' + str(farmingCompanies.iloc[0,0].date()) + ' Grain prices:' + str(wheatDF.iloc[0,0].date()), bbox_to_anchor=(-0.04, 1.15), loc='upper left')
     plt.title(name)
     plt.savefig('analysis/' + name + '.png', dpi=300, bbox_inches='tight') 
     plt.show()
